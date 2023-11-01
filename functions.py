373682e799316962de7e23fe2c5935b70e960d88
RutuThakkar510 <rutu.thakkar@techholding.co>
2023-11-02 01:14:21 +0530

    functions

--- functions.py
+++ functions.py
@@ -3,7 +3,15 @@ def greet():
 
 greet()
 
+# print sum of two no. (passed in args)
 def addition(a: int,b: int):
     print(a + b)
 
 addition(1,2)
+
+# return
+def subtract(a: int,b: int):
+    return a - b
+
+result = subtract(10,5)
+print(result)   
